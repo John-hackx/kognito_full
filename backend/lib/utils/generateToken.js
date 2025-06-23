e50b58122c03719f357c095b8fd8f6a1e3a33081
John Hackx <myofficialsat@gmail.com>
2025-06-23 05:14:52 -0400

    fixed path issue in cookie

--- backend/lib/utils/generateToken.js
+++ backend/lib/utils/generateToken.js
@@ -6,6 +6,7 @@ const generateTokenAndSetCookie = (userId, res) => {
   });
 
   res.cookie("jwt", token, {
+    path: "/",
     httpOnly: true,
     secure: process.env.NODE_ENV === "production", // Use secure cookies in production
     sameSite: "strict", // Helps prevent CSRF attacks
